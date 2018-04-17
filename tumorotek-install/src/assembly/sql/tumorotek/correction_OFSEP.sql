71f0d98c8b660626cc70a0c8c3a305315cb1b59c
Pascal KHIEU <pascal.khieu@walisco.fr>
2018-04-17 20:03:18 +0200

    Import OFSEP (en cours)

---
+++ tumorotek-install/src/assembly/sql/tumorotek/correction_OFSEP.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `CONTEXTE` ADD COLUMN libelle char(50) null;
+ALTER TABLE `PRELEVEMENT` ADD COLUMN date_peremption datetime null;
+UPDATE `CONTEXTE` SET nom='DEFAUT',libelle='Anatomopathologie' WHERE contexte_id = 1;
+UPDATE `CONTEXTE` SET nom='SEROLOGIE',libelle='Sérologie' WHERE contexte_id = 2;
+UPDATE `CONTEXTE` SET nom='BTO',libelle='Banque de tissus osseux' WHERE contexte_id = 3;
+UPDATE `CONTEXTE` SET nom='OFSEP',libelle='OFSEP' WHERE contexte_id = 4;
