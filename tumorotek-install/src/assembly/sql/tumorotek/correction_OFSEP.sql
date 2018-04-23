c87fd5c06c197603139e00e9aa7f8e5c66ee68f1
Pascal KHIEU <pascal.khieu@walisco.fr>
2018-04-23 04:32:41 +0200

    Update SQL Import OFSEP : TABLE_ANNOATATION : INLINE_DISPLAY

--- tumorotek-install/src/assembly/sql/tumorotek/correction_OFSEP.sql
+++ tumorotek-install/src/assembly/sql/tumorotek/correction_OFSEP.sql
@@ -1,3 +1,4 @@
+ALTER TABLE `TABLE_ANNOTATION` ADD COLUMN `INLINE_DISPLAY` bit(1) DEFAULT b'0';
 ALTER TABLE `CONTEXTE` ADD COLUMN libelle char(50) null;
 ALTER TABLE `PRELEVEMENT` ADD COLUMN date_peremption datetime null;
 UPDATE `CONTEXTE` SET nom='DEFAUT',libelle='Anatomopathologie' WHERE contexte_id = 1;
